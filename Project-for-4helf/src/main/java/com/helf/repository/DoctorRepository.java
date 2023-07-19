eeb7607a4cd6a33b1c352af15d07b1785a49a72e
Arvind8874 <105968703+Arvind8874@users.noreply.github.com>
2023-07-19 21:46:14 +0530

    Doctor changed

--- Project-for-4helf/src/main/java/com/helf/repository/DoctorRepository.java
+++ Project-for-4helf/src/main/java/com/helf/repository/DoctorRepository.java
@@ -1,17 +1,15 @@
 package com.helf.repository;
 
-import com.helf.dto.DoctorRequest;
+
 import com.helf.entity.Doctors;
 
 
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
@@ -25,6 +23,7 @@ public interface DoctorRepository extends JpaRepository<Doctors,Long> {
    List<Doctors> getDoctorDetails(@Nullable Long id, @Nullable String mobileNumber, @Nullable String userName);
 //@Query("SELECT d, o FROM Doctors d JOIN d.organization o WHERE d.id = ?1 OR d.mobileNumber = ?2 OR d.userName = ?3")
 //List<Doctors> getDoctorDetails(@Nullable Long id, @Nullable String mobileNumber, @Nullable String userName);
+
 
 
     @Query("SELECT d from Doctors d where d.createdTime >= :startDate AND d.createdTime < :endDate ORDER BY d.modifiedTime DESC")
